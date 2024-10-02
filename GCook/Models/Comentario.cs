b4818210f9c680b6274ae63cc043d7be86169a8b
alanpinheiro96 <alantnt15@gmail.com>
2024-10-01 21:05:42 -0300

    Criaçao de um Projeto de um site de receita usando .Net

---
+++ GCook/Models/Comentario.cs
@@ -0,0 +1,23 @@
+
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GCook.Models;
+
+    [Table("Comentario")]
+    public class Comentario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ReceitaId { get; set; }
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+
+        public DateTime DataComentario { get; set; } = DateTime.Now;
+
+        [StringLength(300)]
+        public string TextoComentario { get; set; }
+    }
